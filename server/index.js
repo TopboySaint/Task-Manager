71a2a770bde09c6366f4a457ffaf08dc1ce04235
TopboySaint <topboysaint1@gmail.com>
2025-08-26 16:03:26 +0100

    Update API base URL in api.js and adjust CORS origin in index.js for deployment

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(cors({
         'http://localhost:5173',
         'http://127.0.0.1:5173',
         'http://localhost:3000',
-        'https://task-manager-frontend-react-iota.vercel.app'
+        'https://task-manager-ten-red.vercel.app/'
     ],
     credentials: true,
 }))
