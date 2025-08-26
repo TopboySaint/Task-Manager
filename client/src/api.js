437bfbc7de8825b33e2cf667ff6210f23b558e37
TopboySaint <topboysaint1@gmail.com>
2025-08-26 16:45:06 +0100

    Fix API base URL by removing trailing slash

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://task-manager-c6h3.onrender.com/",
+  baseURL: "https://task-manager-c6h3.onrender.com",
 });
 
 export default instance;
